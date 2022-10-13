6a768cd1cb5dc2b534da8bbe2b0a97439417c56f
tuanneli <zhuravlev99pic@gmail.com>
2022-10-14 02:54:16 +0300

    shopping page ok

--- user/src/components/customer/ShoppingCart.tsx
+++ user/src/components/customer/ShoppingCart.tsx
@@ -1,20 +1,34 @@
 import React, {useContext} from 'react';
 import {Context} from "../../index";
 import {observer} from "mobx-react-lite";
+import {buttonClasses} from "@mui/material";
 
 const ShoppingCart = () => {
 
     const {shoppingCartStore} = useContext(Context);
 
+    const clearHandler = () => {
+        shoppingCartStore.clear();
+    }
+
     return (
         <div>
             {shoppingCartStore.shoppingCart.map((item) =>
                 <div key={item.name} className="d-flex">
+                    <button onClick={() => shoppingCartStore.removeItemFromShoppingCart(item?.name)}>-</button>
                     <div className='ps-1'>{item?.name}</div>
                     <div className='ps-1'>{item?.price}</div>
                     <div className='ps-1'>{item?.amount}</div>
+                    <button onClick={() => shoppingCartStore.addItemToShoppingCart(item?.name, item?.price)}>+</button>
+                    <div>Стоимость: {item?.amount * Number(item?.price)} тг.</div>
                 </div>
             )}
+            <div>Всего: {shoppingCartStore.shoppingCart.reduce((acc, item) => {
+                return acc + (item?.amount * Number(item?.price))
+            }, 0)} тг.
+            </div>
+            <button onClick={clearHandler}>Очистить</button>
+            <button>Добавить</button>
         </div>
     );
 };
--- user/src/store/ShoppingCartStore.ts
+++ user/src/store/ShoppingCartStore.ts
@@ -23,6 +23,10 @@ export default class ShoppingCartStore {
         return this._amountOfItemsInCart = this._amountOfItemsInCart + 1;
     }
 
+    subtractAmountOfItemsInCart() {
+        return this._amountOfItemsInCart = this._amountOfItemsInCart - 1;
+    }
+
     addItemToShoppingCart(name: string, price: string) {
         this.addAmountOfItemsInCart();
         const isInCart = this._shoppingCart.find(item => item.name === name);
@@ -31,5 +35,22 @@ export default class ShoppingCartStore {
                 item.name === name ? {...item, amount: item.amount + 1} : item)
         }
         this._shoppingCart = [...this._shoppingCart, {name, price, amount: 1}]
+    }
+
+    removeItemFromShoppingCart = (name: string) => {
+        this.subtractAmountOfItemsInCart();
+        this._shoppingCart = this._shoppingCart.reduce((prev, item) => {
+            if (item.name == name) {
+                if (item.amount === 1) return prev;
+                return [...prev, {...item, amount: item.amount - 1}]
+            } else {
+                return [...prev, item]
+            }
+        }, [] as IShoppingCart[])
+    }
+
+    clear() {
+        this._amountOfItemsInCart = 0;
+        this._shoppingCart = [] as IShoppingCart[];
     }
 };
